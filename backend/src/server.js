a913d85dde03d39fad439e1da50864bacc6d8694
Safadini <igorsabadini157@gmail.com>
2026-07-15 11:36:57 -0300

    feat: implement server and main page for API interaction

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,23 @@
 import express from 'express';
+import path from 'path';
+import { execArgv } from 'process';
+import { fileURLToPath } from 'url';
+
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.json()); //habilita o leitor de JSON
+
+const staticPath = path.join(__dirname, '../../fronten/src')
+
+app.use(express.static(staticPath));
 
 const port = 3000; //a porta onde vai rodar
 
 app.get("/", (req, res) => {
-    res.sendFile('index.html', { root: '../frontend' });
+    res.redirect("pages/main")
 });
 
 app.post("/send", (req, res) => {
