4fdab6a93acece8ecfa9e57bfb4f58a045a456d6
Safadini <igorsabadini157@gmail.com>
2026-07-15 16:04:37 -0300

    feat: add temporary data handling and endpoint for form submission

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,13 +20,21 @@ app.get("/", (req, res) => {
     res.redirect("pages/main");
 });
 
+let dadosTemporarios = {}; // Depois conectar DB e pegar dados com querys
+
 app.post("/send", (req, res) => {
     const { email, nome, phone } = req.body;
+
+    dadosTemporarios = { email, nome, phone }; // escopo global da variavel
 
     console.log({ email, nome, phone });
 
     res.status(200).send("OK");
 });
+
+app.get("/send", (req, res) => {
+    res.status(200).json(dadosTemporarios);
+})
 
 app.listen(port, () => {
     console.log(`Server is running in: http://localhost:${port}`);
